327ee38597996de69dfa19260b67df9c0c1ecef1
Nicholas Hillier <nicholas.hillier@lightspeedhq.com>
2018-12-03 10:19:21 -0500

    fixed white space

--- index.js
+++ index.js
@@ -9,62 +9,62 @@ const client = new vision.ImageAnnotatorClient();
 exports.saveTags = (event) => {
   return getKey(event.data).then(key => {
     return processLabels(getStoragePath(event.data), key).then(() => {
-		  console.log('Successfully inserted labels');
-		})
-		.catch(err => {
-		  console.error('Vision API failed', err);
-		});
-	});
+    console.log('Successfully inserted labels');
+  })
+  .catch(err => {
+    console.error('Vision API failed', err);
+    });
+  });
 }
 
 exports.deleteTagger = (event) => {
   return getKey(event.data).then(key => {
-	  return datastore.delete(key).then(() => {
+    return datastore.delete(key).then(() => {
       console.log('Successfully deleted entity.', key);
-	  })
-	  .catch(err => {
-	    console.error('Vision API failed:', err);
-	  });
-	});
+    })
+    .catch(err => {
+      console.error('Vision API failed:', err);
+    });
+  });
 }
 
 function getKey(bucketObject) {
   if(!bucketObject.contentType.startsWith('image/')) {
-		return Promise.reject("object is not an image");
-	}
-				
-	const storagePath = getStoragePath(bucketObject);
-	const query = datastore.createQuery('Images').select('__key__').limit(1);
- 	query.filter('storagePath', '=', storagePath);
+    return Promise.reject("object is not an image");
+  }
 
-	return query.run().then(data => {
+  const storagePath = getStoragePath(bucketObject);
+  const query = datastore.createQuery('Images').select('__key__').limit(1);
+  query.filter('storagePath', '=', storagePath);
+
+  return query.run().then(data => {
     const objectExists = data[0].length > 0;
-		const key = objectExists ? data[0][0][datastore.KEY] : datastore.key('Images');
+    const key = objectExists ? data[0][0][datastore.KEY] : datastore.key('Images');
     return key;
-	})
-	.catch(err => {
-	  console.error("Could not get the targeted bucket:", err);
-	});
+  })
+  .catch(err => {
+    console.error("Could not get the targeted bucket:", err);
+  });
 }
 
-function processLabels(storagePath, key) {		
+function processLabels(storagePath, key) {
   return client.labelDetection(storagePath).then(results => {
-		const labels = results[0].labelAnnotations;
-	  console.log("labels", labels);	
-		const descriptions = labels.filter(label => label.score >= 0.65)
-					.map(label => label.description);
+    const labels = results[0].labelAnnotations;
+    console.log("labels", labels);
+    const descriptions = labels.filter(label => label.score >= 0.65)
+          .map(label => label.description);
     console.info("descriptions", descriptions);
-		return datastore.save({
-		  key: key,
-			data: {
-		    storagePath: storagePath,
-				tags: descriptions 
-			}
-		});
-	})
+    return datastore.save({
+      key: key,
+      data: {
+        storagePath: storagePath,
+        tags: descriptions
+      }
+    });
+  })
 }
 
 function getStoragePath(bucketObject) {
-  return `gs://${bucketObject.bucket}/${bucketObject.name}`; 
+  return `gs://${bucketObject.bucket}/${bucketObject.name}`;
 }
 
