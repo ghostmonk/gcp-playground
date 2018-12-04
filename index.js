9c1c5f86fe90caabf9ff34c8c42a906dc57e9f37
Nicholas Hillier <nicholas.hillier@lightspeedhq.com>
2018-12-03 19:44:01 -0500

    added some function for generating a tmp save

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+const exec = require('child_process');
+const path = require('path');
+const os = require('os');
+const fs = require('fs')
+
 const Datastore = require('@google-cloud/datastore');
 const {Storage} = require('@google-cloud/storage');
 const vision = require('@google-cloud/vision');
 
+
 const datastore = Datastore();
 const storage = new Storage();
 const client = new vision.ImageAnnotatorClient();
+const tmpPath = os.tmpdir();
 
 exports.saveTags = (event) => {
   return getKey(event.data).then(key => {
@@ -54,17 +61,58 @@ function processLabels(storagePath, key) {
     const descriptions = labels.filter(label => label.score >= 0.65)
           .map(label => label.description);
     console.info("descriptions", descriptions);
-    return datastore.save({
+    return {
       key: key,
       data: {
         storagePath: storagePath,
         tags: descriptions
       }
-    });
+    };
   })
 }
 
 function getStoragePath(bucketObject) {
   return `gs://${bucketObject.bucket}/${bucketObject.name}`;
+}
+
+function generateThumbnail(bucketObject) {
+  const filePath = bucketObject.name;
+  const parsedPath = path.parse(bucketObject.name);
+  const fileName = parsedPath.base;
+
+  const bucket = storage.bucket(bucketObject.bucket);
+  const file = bucket.file(bucketObject.name);
+
+  const tempLocalDir = path.join(tmpPath, parsedPath.dir);
+  const tempLocalFile = path.join(tmpLocalDir, fileName);
+
+  return mkDirAsync(tempLocalDir);
+}
+
+function mkDirAsync(dir) {
+  return new Promise((resolve, reject) => {
+    fs.lstat(dir, (err, reject) => {
+      if (err) {
+        if (err.code === 'ENOENT') {
+          fs.mkdir(dir, (err) => {
+            if (err) {
+              reject(err);
+            } else {
+              console.log('created directory');
+              resolve();
+            }
+          });
+        } else {
+          reject(err);
+        }
+      } else {
+        if (stats.isDirectory()) {
+          console.log(`${dir} already exists!`);
+        } else {
+          reject(new Error('A directory was not passed to this function!'));
+        }
+      }
+    })
+  });
 }
 
